a5da18d30aee0108796ebd71b2cddc6120fee72e
tetsu19n1101087 <gizensha0331@gmail.com>
2025-05-10 13:54:42 +0900

    プロフィール画像をcmsから取得

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,27 +3,17 @@ import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
 import { Mail } from 'lucide-react';
 import { GitHubIcon } from '@/components/github-icon';
+import { client } from '@/lib/client';
 
-export default function AboutPage() {
+export default async function AboutPage() {
+  const profile = await client.get({
+    endpoint: 'profile',
+  });
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
       <main className='flex-1'>
-        {/* <section className='w-full py-12 md:py-24 lg:py-32'>
-          <div className='container mx-auto px-4 md:px-6 max-w-7xl'>
-            <div className='flex flex-col items-center justify-center space-y-4 text-center'>
-              <div className='space-y-2'>
-                <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'>
-                  About
-                </h1>
-                <p className='max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400'>
-                  ここにあなたの経歴や自己紹介を書きます。あなたのスキル、経験、情熱について説明してください。
-                  読者があなたについてもっと知りたいと思うような魅力的な内容にしましょう。
-                </p>
-              </div>
-            </div>
-          </div>
-        </section> */}
         <section className='w-full py-12 md:py-24 lg:py-32'>
           <div className='container mx-auto px-4 md:px-6 max-w-7xl'>
             <div className='flex flex-col items-center justify-center space-y-4 text-center mb-12'>
@@ -38,7 +28,7 @@ export default function AboutPage() {
               <div className='md:w-1/3'>
                 <div className='relative w-full aspect-square overflow-hidden rounded-full'>
                   <Image
-                    src='/profile-public.jpg'
+                    src={profile.image.url}
                     alt='プロフィール画像'
                     fill
                     className='object-cover'
@@ -79,12 +69,8 @@ export default function AboutPage() {
                 <h2 className='text-3xl font-bold tracking-tighter sm:text-3xl'>
                   Skill
                 </h2>
-                {/* <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                  私が持っているスキルと技術をご紹介します。
-                </p> */}
               </div>
             </div>
-
             <div className='max-w-4xl mx-auto space-y-8'>
               <div className='space-y-2'>
                 <h3 className='text-xl font-bold'>言語</h3>
@@ -92,7 +78,6 @@ export default function AboutPage() {
                   HTML / CSS / JavaScript / TypeScript / Python
                 </p>
               </div>
-
               <div className='space-y-2'>
                 <h3 className='text-xl font-bold'>フレームワーク</h3>
                 <p className='text-gray-700 dark:text-gray-300'>
@@ -100,21 +85,18 @@ export default function AboutPage() {
                   CSS
                 </p>
               </div>
-
               <div className='space-y-2'>
                 <h3 className='text-xl font-bold'>データベース</h3>
                 <p className='text-gray-700 dark:text-gray-300'>
                   PostgreSQL / MongoDB
                 </p>
               </div>
-
               <div className='space-y-2'>
                 <h3 className='text-xl font-bold'>インフラ</h3>
                 <p className='text-gray-700 dark:text-gray-300'>
                   AWS / Firebase / Vercel
                 </p>
               </div>
-
               <div className='space-y-2'>
                 <h3 className='text-xl font-bold'>データサイエンス</h3>
                 <p className='text-gray-700 dark:text-gray-300'>
@@ -122,7 +104,6 @@ export default function AboutPage() {
                   RStudio
                 </p>
               </div>
-
               <div className='space-y-2'>
                 <h3 className='text-xl font-bold'>その他ツール</h3>
                 <p className='text-gray-700 dark:text-gray-300'>
@@ -141,7 +122,6 @@ export default function AboutPage() {
                 </h2>
               </div>
             </div>
-
             <div className='max-w-xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6'>
               <a
                 href='https://github.com/tetsu19n1101087'
@@ -153,7 +133,6 @@ export default function AboutPage() {
                 <h3 className='font-medium'>GitHub</h3>
                 <p className='text-sm text-gray-500'>@tetsu19n1101087</p>
               </a>
-
               <a
                 href='mailto:22l1073z@student.gs.chiba-u.jp'
                 className='flex flex-col items-center p-6 bg-white dark:bg-gray-900 rounded-lg shadow-sm hover:shadow-md transition-shadow'
