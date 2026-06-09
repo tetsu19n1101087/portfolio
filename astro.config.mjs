650a3c2c31e17df1b3f06c094a52f16b8c9ae5ba
tetsu19n1101087 <gizensha0331@gmail.com>
2026-06-09 11:18:39 +0900

    works ページの作成
    ・作品一覧ページと詳細ページを暫定的なデザインで作成
    ・更新用のvercel の isr 設定の追加
    ・vercel の analyticsの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig, envField } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    isr: {
+      expiration: 60 * 60 * 24,
+    },
+  }),
   env: {
     schema: {
       CMS_SERVICE_DOMAIN: envField.string({
